b650a9cf162d462a122bba345bd030995229defe
josegoval <56361627+josegoval@users.noreply.github.com>
2020-08-22 19:48:23 +0200

    css loader and readme requirements
    Css-loader, MiniCssExtractor and Style-loader dev packages added.
    Readme with requirements added.
    
    Changes to be committed:
    	modified:   README.md
    	modified:   package.json
    	modified:   src/index.html
    	new file:   src/index.js
    	new file:   src/style.css
    	modified:   webpack.config.js
    	modified:   yarn.lock

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BasicForm
+
 A Basic HTML form handling inputs
+
+# Requirements
+
+1. Inputs
+   - firstname? (letters and spaces)
+   - lastname? (letters and spaces)
+   - username (Letters and underscores)
+   - password (> 8 chars and not only numbers)
+   - repeatPassword
+2. Error feedbacks
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
